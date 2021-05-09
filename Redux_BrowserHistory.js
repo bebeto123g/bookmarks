3cb884bd7dca1f003f621b8774095fc039d810a6
bebeto123g <65464279+bebeto123g@users.noreply.github.com>
2021-05-09 08:39:28 +0300

    Create Redux_BrowserHistory.js

---
+++ Redux_BrowserHistory.js
@@ -0,0 +1,28 @@
+import { combineReducers } from 'redux'
+import { connectRouter } from 'connected-react-router'
+import { phonesReducer } from './phonesReducer'
+import { phonesPageReducer } from './phonesPageReducer'
+
+export const rootReducer = (history) =>
+    combineReducers({
+        router: connectRouter(history),
+        phonesReducer,
+        phonesPageReducer,
+    })
+
+import { createStore, applyMiddleware } from 'redux'
+import { createBrowserHistory } from 'history'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { routerMiddleware } from 'connected-react-router'
+import thunk from 'redux-thunk'
+
+import { rootReducer } from './reducers'
+
+export const history = createBrowserHistory()
+
+const middlewares = [thunk, routerMiddleware(history)]
+
+export const store = createStore(
+    rootReducer(history),
+    composeWithDevTools(applyMiddleware(...middlewares))
+)
