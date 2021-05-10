f7b3725d7539b88b0ce1d7aa0115fdfcb07a9f99
bebeto123g <65464279+bebeto123g@users.noreply.github.com>
2021-05-10 11:14:03 +0300

    Create redux-logger.js

---
+++ redux-logger.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { createStore, applyMiddleware, compose } from 'redux'
+import rootReducer from './redux/rootReducer'
+import { Provider } from 'react-redux'
+import reduxThunk from 'redux-thunk'
+
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose
+
+const loggerMiddleware = (store) => (next) => (action) => {
+  const result = next(action)
+  console.log('Middleware', store.getState())
+  return result
+}
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(loggerMiddleware, reduxThunk))
+)
+
+const app = (
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root'))
