2e37dae3e52adb381b8afa5887e62122bd76bc42
bebeto123g <65464279+bebeto123g@users.noreply.github.com>
2021-08-13 11:29:15 +0300

    Update wave.js

--- wave.js
+++ wave.js
@@ -1,1 +1,24 @@
+function createWaveCircle(event) {
+    const element = event.currentTarget;
 
+    const circle = document.createElement('span');
+    const diameter = Math.max(element.clientWidth, element.clientHeight);
+    const radius = diameter / 2;
+
+    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.left = `${event.pageX - element.offsetLeft - radius}px`;
+    circle.style.top = `${event.pageY - element.offsetTop - radius}px`;
+    circle.classList.add('wave-element__circle');
+
+    const waveElement = element.querySelectorAll('.wave-element__circle')[0];
+
+    if (waveElement) {
+        waveElement.remove();
+    }
+
+    element.appendChild(circle);
+}
+
+document.querySelectorAll('.wave-element').forEach((el) => {
+    el.addEventListener('pointerup', createWaveCircle);
+});
