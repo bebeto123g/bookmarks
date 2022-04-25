8ead655f88de37a5d13e9c8dd972b4acf410692d
bebeto123g <65464279+bebeto123g@users.noreply.github.com>
2022-04-25 17:39:34 +0300

    Create Display.js

---
+++ Display.js
@@ -0,0 +1,64 @@
+class Display {
+    static SMALL_MOBILE = 'small-mobile';
+    static MOBILE = 'mobile';
+    static TABLET = 'tablet';
+    static SMALL_DESKTOP = 'small-desktop';
+    static DESKTOP = 'desktop';
+
+    static BREAKPOINT = {
+        [Display.MOBILE]: 500,
+        [Display.TABLET]: 640,
+        [Display.SMALL_DESKTOP]: 992,
+        [Display.DESKTOP]: 1200,
+    };
+
+    static isMobile() {
+        return window.innerWidth < Display.BREAKPOINT[Display.MOBILE];
+    }
+
+    static isSmallTablet() {
+        return (
+            window.innerWidth > Display.BREAKPOINT[Display.MOBILE] &&
+            window.innerWidth < Display.BREAKPOINT[Display.TABLET]
+        );
+    }
+
+    static isTablet() {
+        return (
+            window.innerWidth > Display.BREAKPOINT[Display.TABLET] &&
+            window.innerWidth < Display.BREAKPOINT[Display.SMALL_DESKTOP]
+        );
+    }
+
+    static isSmallDesktop() {
+        return (
+            window.innerWidth > Display.BREAKPOINT[Display.SMALL_DESKTOP] &&
+            window.innerWidth < Display.BREAKPOINT[Display.DESKTOP]
+        );
+    }
+
+    static isDesktop() {
+        return window.innerWidth > Display.BREAKPOINT[Display.DESKTOP];
+    }
+
+    static getTypeDisplayWidth() {
+        switch (true) {
+            case Display.isMobile():
+                return Display.MOBILE;
+            case Display.isSmallTablet():
+                return Display.SMALL_TABLET;
+            case Display.isTablet():
+                return Display.TABLET;
+            case Display.isSmallDesktop():
+                return Display.SMALL_DESKTOP;
+            case Display.isDesktop():
+                return Display.DESKTOP;
+            default:
+                return false;
+        }
+    }
+
+    static isMinWidthBreakpoint(display) {
+        return window.innerWidth < Display.BREAKPOINT[display];
+    }
+}
