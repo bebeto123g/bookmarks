a27c62609e7e0b9938e0103af654c44d363b8bb6
bebeto123g <65464279+bebeto123g@users.noreply.github.com>
2022-04-27 18:50:16 +0300

    Update IntersectionObserver.js

--- IntersectionObserver.js
+++ IntersectionObserver.js
@@ -22,3 +22,22 @@ const imageObserver = new IntersectionObserver(callback, options);
 document.querySelectorAll('[data-src]').forEach((elemet) => {
     imageObserver.observe(elemet);
 });
+========================================================================
+
+const observer = new IntersectionObserver(observerCallback, { threshold: 0.1 });
+
+document.querySelectorAll('[data-js-role="lazy"]').forEach((element) => {
+    observer.observe(element);
+});
+
+function observerCallback(entries, observer) {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+            return;
+        }
+        if (entry.target.dataset.src) {
+            entry.target.src = `${entry.target.dataset.src}`;
+        }
+        observer.unobserve(entry.target);
+    });
+}
